1385537abf1475a34877ec2e5ceb4bc0a533494d
NguyenGiangMinh <anhminhne14@gmail.com>
2025-11-01 16:57:45 +0700

    user-service

--- user-service/src/app.controller.ts
+++ user-service/src/app.controller.ts
@@ -1,12 +1,41 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('create_user')
+  createUser(@Payload() message: any) {
+    const createUserDto: CreateUserDto = message.value;
+    return this.appService.createUser(createUserDto);
   }
-}
+
+  @MessagePattern('get_user')
+  getUser(@Payload() message: any) {
+    const { id } = message.value;
+    return this.appService.getUserById(id);
+  }
+
+  @MessagePattern('update_user')
+  updateUser(@Payload() message: any) {
+    const { id, dto } = message.value;
+    const updateUserDto: UpdateUserDto = dto;
+    return this.appService.updateUser(id, updateUserDto);
+  }
+
+  @MessagePattern('delete_user')
+  deleteUser(@Payload() message: any) {
+    const { id } = message.value;
+    return this.appService.deleteUser(id);
+  }
+
+  @MessagePattern('get_all_users')
+  getAllUsers() {
+    return this.appService.getAll();
+  }
+} 
+ 
--- user-service/src/app.service.ts
+++ user-service/src/app.service.ts
@@ -2,16 +2,41 @@ import { Injectable } from '@nestjs/common';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class AppService {
-  crateUser(createUserDto: CreateUserDto): User {
+  private users: User[] = [];
+
+  createUser(createUserDto: CreateUserDto): User {
     const newUser: User = {
-      id: 
-    }
+      id: randomUUID(),
+      ...createUserDto
+    };
+
+    this.users.push(newUser);
+    return newUser;
   }
 
   getUserById(id: string): User | undefined {
-    return this.getUserById.
+    return this.users.find(user => user.id === id);
+  }
+
+  updateUser(id: string, dto: UpdateUserDto): User | undefined {
+    const user = this.getUserById(id);
+    if (!user) return undefined;
+    Object.assign(user, dto);
+    return user;
+  }
+
+  deleteUser(id: string): boolean {
+    const index = this.users.findIndex(user => user.id === id);
+    if (index === -1) return false;
+    this.users.splice(index, 1);
+    return true;
+  }
+
+  getAll(): User[] {
+    return this.users;
   }
 }
